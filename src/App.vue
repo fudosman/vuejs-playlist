6a01a50f19785424ffb96fbf46b73abb469e84c2
afudoh kachi <fudosman@gmail.com>
2021-01-26 14:21:26 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Footer from './components/Footer.vue'
+import Ninjas from './components/Ninjas.vue'
+
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas,
+  },
+  data () {
+    return {
+      title:'Ninja App'
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
